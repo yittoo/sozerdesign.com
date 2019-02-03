9b5639a664e4197bbd840b9a67fc91d005cb885f
yittoo <yittoo@users.noreply.github.com>
2019-02-03 19:04:45 +0300

    add bootstrap jquery, fix landing

--- app.js
+++ app.js
@@ -4,18 +4,16 @@ var express = require("express"),
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
-app.use(bodyParser.urlencoded({extended: true}));
-
 app.get("/:lang", function(req, res){
     if(req.params.lang === "en" || req.params.lang === "tr"){
         res.render("index.ejs", {language: req.params.lang});
     } else if(req.params.lang !== "favicon.ico") {
-        console.log("that language support does not exist");
         res.redirect("/");
     }
 });
