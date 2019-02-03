81e81adeef78555eca93e6fff9b7af2b8331452b
yittoo <yittoo@users.noreply.github.com>
2019-02-03 16:17:07 +0300

    edit initial overlook, setup initial foldering

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# testWeb" 
+My own opensource design for my portfolio website.
+
+Entry Point: app.js
+
+Feel free to use it.
--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 var express = require("express"),
-    app     = express();
+    app     = express(),
+    bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req, res){
-    res.render("home");
+    res.render("landing");
 });
 
-app.get("/about", function(req, res){
-    res.render("about");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/:lang", function(req, res){
+    if(req.params.lang === "en" || req.params.lang === "tr"){
+        res.render("index.ejs", {language: req.params.lang});
+    } else if(req.params.lang !== "favicon.ico") {
+        console.log("that language support does not exist");
+        res.redirect("/");
+    }
 });
 
+    
 
 app.listen(3000, "127.0.0.1" , function(){
     console.log("webbo is uppo");
