7c545772cceb5b24e88b03067e9a34124a57aa90
yittoo <yittoo@users.noreply.github.com>
2019-02-04 17:31:27 +0300

    additional edits to index en

--- app.js
+++ app.js
@@ -10,15 +10,25 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
-app.get("/:lang", function(req, res){
-    if(req.params.lang === "en" || req.params.lang === "tr"){
-        res.render("index.ejs", {language: req.params.lang});
-    } else if(req.params.lang !== "favicon.ico") {
-        res.redirect("/");
-    }
+app.get("/en", function(req, res){
+    res.render("en/index.ejs");
 });
 
-    
+app.get("/tr", function(req, res){
+    res.render("tr/index.ejs");
+});
+
+app.get("/tr/*", function(req, res){
+    res.redirect("/tr");
+});
+
+app.get("/en/*", function(req, res){
+    res.redirect("/en");
+});
+
+app.get("*", function(req, res){
+    res.redirect("/");
+});
 
 app.listen(3000, "127.0.0.1" , function(){
     console.log("webbo is uppo");
--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,9 @@
-ECHO is on.
+$("#about").on("click", function(){
+    $("#index-main-section").addClass("d-none");
+    $("#about-section").removeClass("d-none");
+});
+
+$(".btn-go-back").on("click", function(){
+    $("#about-section").addClass("d-none");
+    $("#index-main-section").removeClass("d-none");
+});
