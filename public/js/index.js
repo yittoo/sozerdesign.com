c39427c7172433f9af084f592850dc4afdb3db47
yittoo <yittoo@users.noreply.github.com>
2019-02-05 17:05:57 +0300

    add animation, keep index of section

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,44 @@
+inIt();
+
+var mainToSection = {
+    toAbout: {id: "#about-section", index: 0},
+    toSkills: {id: "#skills-section", index: 1},
+    toEducation: {id: "#education-section", index: 2},
+    toPersonal: {id: "#personal-section", index: 3},
+    toPortfolio: {id: "#portfolio-section", index: 4},
+    toContact: {id: "#contact-section", index: 5},
+}
+
 $(".btn-home").on("click", function(){
-    $("#about-section").addClass("d-none");
-    $("#navbar").addClass("d-none");
-    $("#index-main-section").removeClass("d-none");
+    $("#index-parent").children().fadeOut(500);
+    $("#navbar").fadeOut(500, function(){
+        $("#index-main-section").addClass("slide-rotate-hor-top-reverse");
+        $("#index-main-section").fadeIn(500, function(){
+            $("#index-main-section").removeClass("slide-rotate-hor-top-reverse");
+            currentIndex = -1;
+        });
+    });
 });
 
-$(".toAbout").on("click", function(){
-    $("#index-main-section").addClass("d-none");
-    $("#about-section").removeClass("d-none");
-    $("#navbar").removeClass("d-none");
+$(".main-btn-group").on("click", function(){
+    var hitSection = mainToSection[this.id];
+    $("#index-main-section").addClass("slide-rotate-hor-top");
+    $("#index-main-section").fadeOut(500, function(){
+        $("#index-main-section").removeClass("slide-rotate-hor-top");
+        botToMid($("#navbar"));
+        botToMid($(hitSection.id));
+        currentIndex = hitSection.index;
+    });
 });
 
-$(".toAbout").on("click", function(){
-    $("#index-main-section").addClass("d-none");
-    $("#about-section").removeClass("d-none");
-    $("#navbar").removeClass("d-none");
-});
+function botToMid(ele){
+    ele.removeClass("d-none");
+    ele.css("display", "none");
+    ele.fadeIn(500);
+}
+
+function inIt(){
+    $("#index-main-section").css("display", "none");
+    $("#index-main-section").fadeIn(700);
+    var currentIndex = -1;
+};
